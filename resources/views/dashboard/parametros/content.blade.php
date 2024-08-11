5f94b2fbe2f93635bb98bbb3c6409224603c02ea
Yonathan Castillo <leothan522@gmail.com>
2024-08-11 18:46:16 -0400

    Composer update | Responsive mejorado Parametros y Usuarios

--- resources/views/dashboard/parametros/content.blade.php
+++ resources/views/dashboard/parametros/content.blade.php
@@ -1,33 +1,19 @@
 <div class="row justify-content-center">
 
-    <div class="col-md-4">
-        @include('dashboard.parametros.form')
-    </div>
-
-    <div class="col-md-8">
-        @include('dashboard.parametros.table')
-    </div>
-
-</div>
-<div class="row justify-content-center">
-    <div class="col-md-12">
-        <label for="">Parametros Manuales</label>
+    <div class="col-md-4 col-lg-3">
+        <label>Parametros Manuales</label>
         <ul>
             <li>numRowsPaginate[null|numero]</li>
             <li>size_codigo[tamaño|null]</li>
-            <li>proximo_codigo_ajustes[empresa_id|int]</li>
-            <li>formato_codigo_ajustes[empresa_id|text]</li>
-            <li>editable_codigo_ajustes[empresa_id|1/0]</li>
-            <li>editable_fecha_ajustes[empresa_id|1/0]</li>
-            <li>proximo_codigo_planificacion[empresa_id|int]</li>
-            <li>formato_codigo_planificacion[empresa_id|text]</li>
-            <li>proximo_codigo_despachos[empresa_id|int]</li>
-            <li>formato_codigo_despachos[empresa_id|text]</li>
-            <li>editable_codigo_despachos[empresa_id|1/0]</li>
-            <li>editable_fecha_despachos[empresa_id|1/0]</li>
             {{--<li>iva</li>
             <li>telefono_soporte</li>
             <li>codigo_pedido</li>--}}
         </ul>
     </div>
+
+    <div class="col-md-8 col-lg-9">
+        @include('dashboard.parametros.table')
+        @include('dashboard.parametros.modal')
+    </div>
+
 </div>
